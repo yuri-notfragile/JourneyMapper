a9838e3763168c34234391385f91bcf42fc90ddb
Yuri Lim <131278132+yuri-notfragile@users.noreply.github.com>
2024-02-24 21:28:59 +0900

    Update script_login.js

--- script_login.js
+++ script_login.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// 회원가입 및 로그인 페이지에서 쓰는 함수들
 const loginButton = document.querySelector('.login-toggle');
 const signupButton = document.querySelector('.signup-toggle');
 const loginSlide = document.querySelector('.slide.login');
@@ -9,41 +8,32 @@ const signupSlide = document.querySelector('.slide.signup');
 const loginForm = document.querySelector('.slide.login form');
 const signupForm = document.querySelector('.slide.signup form');
 
-// 로그인 버튼 클릭 시
 loginButton.addEventListener('click', () => {
-  // 로그인 슬라이드 표시, 회원가입 슬라이드 숨김
   loginSlide.style.display = 'block';
   signupSlide.style.display = 'none';
-  // 버튼 색상 전환
   loginButton.classList.add('active');
   signupButton.classList.remove('active');
 });
 
-// 회원가입 버튼 클릭 시
 signupButton.addEventListener('click', () => {
-  // 로그인 슬라이드 숨김, 회원가입 슬라이드 표시
   loginSlide.style.display = 'none';
   signupSlide.style.display = 'block';
-  // 버튼 색상 전환
   loginButton.classList.remove('active');
   signupButton.classList.add('active');
 });
 
-// 검색 페이지로 이동하는 함수
 function redirectToSearchPage() {
   window.location.href = 'http://127.0.0.1:5500/page_search.html';
 }
 
 async function handleSignup(event) {
-    event.preventDefault(); // 폼의 기본 동작을 막음
+    event.preventDefault();
   
-    // 입력값 가져오기
     const id = signupForm.querySelector('input[type="text"]').value;
     const nickname = signupForm.querySelector('input[type="text"][placeholder="Nickname"]').value;
     const password = signupForm.querySelector('input[type="password"]').value;
     const confirmPassword = signupForm.querySelector('input[type="password"][placeholder="Confirm Password"]').value;
   
-    // 패스워드 확인
     if (password !== confirmPassword) {
       alert('패스워드가 일치하지 않습니다.');
       return;
@@ -55,7 +45,6 @@ async function handleSignup(event) {
         password: 'traveler01',
        }
   
-    // 하드코딩된 회원가입 정보와 비교
     if (id === hardcodedsignup.id && password === hardcodedsignup.password) {
       alert('회원가입 성공!, 로그인 해주세요');
     } else {
@@ -63,11 +52,9 @@ async function handleSignup(event) {
     }
   }
   
-  // 로그인 처리 함수
   async function handleLogin(event) {
-    event.preventDefault(); // 폼의 기본 동작을 막음
+    event.preventDefault();
   
-    // 입력값 가져오기
     const id = loginForm.querySelector('input[type="text"]').value;
     const password = loginForm.querySelector('input[type="password"]').value;
     
@@ -76,7 +63,6 @@ async function handleSignup(event) {
         password: 'traveler01',
       };
   
-    // 하드코딩된 로그인 정보와 비교
     if (id === hardcodedlogin.id && password === hardcodedlogin.password) {
       alert('로그인 성공!');
       redirectToSearchPage();
@@ -85,6 +71,6 @@ async function handleSignup(event) {
     }
   }
   
-  // 이벤트 리스너 할당
+
   signupForm.addEventListener('submit', handleSignup);
   loginForm.addEventListener('submit', handleLogin);
